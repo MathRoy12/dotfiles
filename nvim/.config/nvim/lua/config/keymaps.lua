0de84da2dd304029bbf991cdac26a91b666123ad
Mathieu Roy <131190408+MathRoy12@users.noreply.github.com>
2026-03-10 14:39:47 -0400

    add a

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -5,5 +5,5 @@ vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right win
 vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
 vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
 vim.keymap.set({ "n", "v", "i" }, "<C-s>", function()
-	vim.cmd("w")
+	vim.cmd("wa")
 end, { desc = "save" })
